b0a3026d8a60c7449e9696fd455a40d526ddcbcc
Nithin-s-gowda <nithingowdasn12@gmail.com>
2024-12-22 20:44:47 +0530

    Updated files and changes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://jxycdjsqckvjgcmpjmmk.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4eWNkanNxY2t2amdjbXBqbW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4NTE1OTYsImV4cCI6MjA1MDQyNzU5Nn0.GWGr1VcWVM3gUGVkwhhyCz3kGZOAGHbBzaRCc2TDipg'
+
+};
